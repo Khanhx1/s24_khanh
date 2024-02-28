7e20828d9de341c006f6e1bb02ec1e0cad652c72
Khanhx1 <nguyentukhanh2001@gmail.com>
2024-02-28 22:58:47 +0700

    fix

--- Module_5_K/ss1_es6/bt_them/bt3.js
+++ Module_5_K/ss1_es6/bt_them/bt3.js
@@ -5,10 +5,8 @@ let student = {
     english: 'English'
 
 }
-let getInfo = (person) => {
-    person.firstName === undefined ? console.log("firstName: Quân") : console.log(`firstName: ${person.firstName}`);
-    person.degree === undefined ? console.log("Degree: Na") : console.log(`Degree: ${person.degree}`);
-    return ""
+function displayInfo({ firstName = "Quân", degree = "N/A" }) {
+    console.log(`firstName: ${firstName}, degree: ${degree}`);
 }
 
-console.log(getInfo(student));
+console.log(displayInfo(student));
