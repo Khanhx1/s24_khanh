ee8396df7dcc5092a9f3c18680cd11b81e2ccd8f
Khanhx1 <nguyentukhanh2001@gmail.com>
2024-03-01 14:13:13 +0700

    k

--- Module_5_K/ss2_react_js/bt_1/src/App.js
+++ Module_5_K/ss2_react_js/bt_1/src/App.js
@@ -5,31 +5,37 @@ import React from "react";
 function App() {
   const students = [
     {
+      id: 1,
       company: 'Alfreds Futterkiste',
       contact: 'Maria Anders',
       country: 'Germany'
     },
     {
+      id: 2,
       company: 'Centro comercial Moctezuma',
       contact: 'Francisco Chang',
       country: 'Mexico'
     },
     {
+      id: 3,
       company: 'Ernst Handel',
       contact: 'Roland Mendel',
       country: 'Austria'
     },
     {
+      id: 4,
       company: 'Island Trading',
       contact: 'Helen Bennett',
       country: 'UK'
     },
     {
+      id: 5,
       company: 'Laughing Bacchus Winecellars',
       contact: 'Yoshi Tannamuri',
       country: 'Canada'
     },
     {
+      id: 6,
       company: 'Magazzini Alimentari Riuniti',
       contact: 'Giovanni Rovelli',
       country: 'Italy'
@@ -44,7 +50,7 @@ function App() {
             <th>Contry</th>
           </tr>
           {students.map(student => (
-              <tr>
+              <tr key={student.id}>
                 <td>{student.company}</td>
                 <td>{student.contact}</td>
                 <td>{student.country}</td>
